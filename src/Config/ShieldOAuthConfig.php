ba899f239b5149b555576f9ab7a57b08b5a47f1a
Pooya Parsa Dadashi <Pooya_parsa_dadashi@yahoo.com>
2022-11-10 16:12:36 +0330

    docs: fix phpstan error from `Config/ShieldOAuthConfig.php`

--- src/Config/ShieldOAuthConfig.php
+++ src/Config/ShieldOAuthConfig.php
@@ -17,6 +17,20 @@ use CodeIgniter\Config\BaseConfig;
 
 class ShieldOAuthConfig extends BaseConfig
 {
+    /**
+     * --------------------------------------------------------------------------
+     * OAuth Configs
+     * --------------------------------------------------------------------------
+     *
+     * Set keys and active any OAuth
+     *
+     * Here you can set the keys received from any OAuth servers.
+     * for more information on getting keys:
+     *
+     * @see https://github.com/datamweb/shield-oauth/blob/develop/docs/get_keys.md
+     *
+     * @var array<string, array<string, bool|string>>
+     */
     public array $oauthConfigs = [
         'github' => [
             'client_id'     => 'Get it from GitHub',
@@ -38,38 +52,52 @@ class ShieldOAuthConfig extends BaseConfig
         // ],
     ];
 
-    /*
-    * If you use different names for the columns in the users table, use the following settings.
-
-    * Data of Table "users":
-    * +----+----------+--------+...+------------+-----------+--------+
-    * | id | username | status |...| first_name | last_name | avatar |
-    * +----+----------+--------+...+------------+-----------+--------+
-    * In fact, you set in which column the information received from the services should be recorded.
-    * For example, the first name received from OAuth should be recorded in column 'first_name' of the 'users' table shield.
-    *
-    * NOTE :
-    *       This is suitable for those who have already installed the shield and created their own columns.
-    *       In this case, there is no need to execute `php spark migrate -n Datamweb\ShieldOAuth`.
-    *       Just set the following values with your table columns.
-    */
+    /**
+     * --------------------------------------------------------------------------
+     * Users Columns Name
+     * --------------------------------------------------------------------------
+     * If you use different names for the columns in the users table, use the following settings.
+     *
+     * Data of Table "users":
+     * +----+----------+--------+...+------------+-----------+--------+
+     * | id | username | status |...| first_name | last_name | avatar |
+     * +----+----------+--------+...+------------+-----------+--------+
+     * In fact, you set in which column the information received from the OAuth services should be recorded.
+     * For example, the first name received from OAuth should be recorded in column 'first_name' of the 'users' table shield.
+     * NOTE :
+     *       This is suitable for those who have already installed the shield and created their own columns.
+     *       In this case, there is no need to execute `php spark migrate -n Datamweb\ShieldOAuth`.
+     *       Just set the following values with your table columns.
+     *
+     * @var array<string, string>
+     */
     public array $usersColumnsName = [
         'first_name' => 'first_name',
         'last_name'  => 'last_name',
         'avatar'     => 'avatar',
     ];
 
-    /*
-    * If the user is already registered, by default when trying to login, he/she information will be synchronized.
-    * if you want to cancel it, set false.
-    */
+    /**
+     * --------------------------------------------------------------------------
+     * Syncing User Info
+     * --------------------------------------------------------------------------
+     * Turn ON/OFF user data update
+     *
+     * If the user is already registered, by default when trying to login, he/she information will be synchronized.
+     * if you want to cancel it, set false.
+     */
     public bool $syncingUserInfo = true;
 
-    /*
-    * When the user login with his profile, the OAuth server directs him to the following path.
-    * So change this value only when you need to make an order.
-    * By default it returns to the following path:
-    * http:\\localhost:8080\oauth\call-back
-    */
+    /**
+     * --------------------------------------------------------------------------
+     * Call Back Route
+     * --------------------------------------------------------------------------
+     * Set your custom call-back name
+     *
+     * When the user login with his profile, the OAuth server directs him to the following path.
+     * So change this value only when you need to make an customez.
+     * By default it returns to the following path:
+     * http:\\localhost:8080\oauth\call-back
+     */
     public string $call_back_route = 'call-back';
 }
