87a8eafebaa05ab151268200745a0e1a59d0b2dd
kenjis <kenji.uui@gmail.com>
2023-08-23 15:04:21 +0900

    chore: add baseline for PHPStan
    vendor/bin/phpstan analyse -b phpstan-baseline.php

---
+++ phpstan-baseline.php
@@ -0,0 +1,188 @@
+<?php declare(strict_types = 1);
+
+$ignoreErrors = [];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Commands\\\\OAuthSetup\\:\\:copyAndReplace\\(\\) has parameter \\$replaces with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Commands/OAuthSetup.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Commands\\\\OAuthSetup\\:\\:\\$arguments type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Commands/OAuthSetup.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Commands\\\\OAuthSetup\\:\\:\\$distPath has no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Commands/OAuthSetup.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Config\\\\Registrar\\:\\:Generators\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Config/Registrar.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Call to deprecated function random_string\\(\\)\\:
+The type \'basic\', \'md5\', and \'sha1\' are deprecated\\. They are not cryptographically secure\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Controllers/OAuthController.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Class Datamweb\\\\ShieldOAuth\\\\Controllers\\\\LoginModel not found\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Controllers/OAuthController.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Controllers\\\\OAuthController\\:\\:checkExistenceUser\\(\\) has parameter \\$find with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Controllers/OAuthController.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Controllers\\\\OAuthController\\:\\:syncingUserInfo\\(\\) has parameter \\$find with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Controllers/OAuthController.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Controllers\\\\OAuthController\\:\\:syncingUserInfo\\(\\) has parameter \\$updateFildes with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Controllers/OAuthController.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Access to an undefined property Datamweb\\\\ShieldOAuth\\\\Libraries\\\\Basic\\\\AbstractOAuth\\:\\:\\$token\\.$#',
+	'count' => 2,
+	'path' => __DIR__ . '/src/Libraries/Basic/AbstractOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Libraries\\\\Basic\\\\AbstractOAuth\\:\\:fetchAccessTokenWithAuthCode\\(\\) has parameter \\$allGet with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/Basic/AbstractOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Libraries\\\\Basic\\\\AbstractOAuth\\:\\:getColumnsName\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/Basic/AbstractOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Libraries\\\\Basic\\\\AbstractOAuth\\:\\:getUserInfo\\(\\) has parameter \\$allGet with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/Basic/AbstractOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Libraries\\\\Basic\\\\AbstractOAuth\\:\\:setColumnsName\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/Basic/AbstractOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Call to deprecated function random_string\\(\\)\\:
+The type \'basic\', \'md5\', and \'sha1\' are deprecated\\. They are not cryptographically secure\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GithubOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GithubOAuth\\:\\:fetchAccessTokenWithAuthCode\\(\\) has parameter \\$allGet with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GithubOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GithubOAuth\\:\\:setColumnsName\\(\\) has parameter \\$userInfo with no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GithubOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GithubOAuth\\:\\:setColumnsName\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GithubOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GithubOAuth\\:\\:\\$API_CODE_URL has no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GithubOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GithubOAuth\\:\\:\\$API_TOKEN_URL has no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GithubOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GithubOAuth\\:\\:\\$API_USER_INFO_URL has no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GithubOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GithubOAuth\\:\\:\\$APPLICATION_NAME has no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GithubOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GithubOAuth\\:\\:\\$client has no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GithubOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GithubOAuth\\:\\:\\$config has no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GithubOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Variable \\$usersColumnsName might not be defined\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GithubOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Call to deprecated function random_string\\(\\)\\:
+The type \'basic\', \'md5\', and \'sha1\' are deprecated\\. They are not cryptographically secure\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GoogleOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GoogleOAuth\\:\\:fetchAccessTokenWithAuthCode\\(\\) has parameter \\$allGet with no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GoogleOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GoogleOAuth\\:\\:setColumnsName\\(\\) has parameter \\$userInfo with no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GoogleOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Method Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GoogleOAuth\\:\\:setColumnsName\\(\\) return type has no value type specified in iterable type array\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GoogleOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GoogleOAuth\\:\\:\\$API_CODE_URL has no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GoogleOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GoogleOAuth\\:\\:\\$API_TOKEN_URL has no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GoogleOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GoogleOAuth\\:\\:\\$API_USER_INFO_URL has no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GoogleOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GoogleOAuth\\:\\:\\$APPLICATION_NAME has no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GoogleOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GoogleOAuth\\:\\:\\$client has no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GoogleOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Property Datamweb\\\\ShieldOAuth\\\\Libraries\\\\GoogleOAuth\\:\\:\\$config has no type specified\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GoogleOAuth.php',
+];
+$ignoreErrors[] = [
+	'message' => '#^Variable \\$usersColumnsName might not be defined\\.$#',
+	'count' => 1,
+	'path' => __DIR__ . '/src/Libraries/GoogleOAuth.php',
+];
+
+return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
