66a9fe7b2155c311a316f9811a914ef5ea36b268
kenjis <kenji.uui@gmail.com>
2023-08-24 10:57:31 +0900

    chore: update .php-cs-fixer.dist.php for rector

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -22,7 +22,10 @@ $finder = Finder::create()
         // __DIR__ . '/tests/',
     ])
     ->exclude('build')
-    ->append([__FILE__]);
+    ->append([
+        __FILE__,
+        __DIR__ . '/rector.php',
+    ]);
 
 $overrides = [
     'declare_strict_types' => true,
