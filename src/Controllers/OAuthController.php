f9b7afd1fe7f8fe43f1b50bb5d6d61537355d207
Aselsan <syamsuddine9@gmail.com>
2024-05-17 01:46:30 +0700

    Update OAuthController.php

--- src/Controllers/OAuthController.php
+++ src/Controllers/OAuthController.php
@@ -141,7 +141,7 @@ class OAuthController extends BaseController implements ControllersInterface
      *
      * @param array $find           Array containing criteria to find the user.
      * @param array $updateFields   Fields to update for the user.
-     *
+     *                              Missing type for iterable values in $updateFields array.
      * @return int                  The ID of the user whose information is synced.
      */
     private function syncingUserInfo(array $find = [], array $updateFields = []): int
