346473e15b47de8bf0bc37aef80efc2272420aa4
Aselsan <syamsuddine9@gmail.com>
2024-05-17 02:31:02 +0700

    Update OAuthController.php

--- src/Controllers/OAuthController.php
+++ src/Controllers/OAuthController.php
@@ -144,7 +144,7 @@ class OAuthController extends BaseController implements ControllersInterface
      *
      * @return int The ID of the user whose information is synced
      */
-    private function syncingUserInfo(array $find = [], array $updateFields = []): int // @phpstan-ignore-line
+    private function syncingUserInfo(array $find = [], array $updateFields = []): int
     {
         $users = model('ShieldOAuthModel');
         $user  = $users->findByCredentials($find);
