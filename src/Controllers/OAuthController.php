17be126bf2e43cc7a0e560d6d251a93b41fe7b57
Aselsan <syamsuddine9@gmail.com>
2024-05-17 01:39:21 +0700

    Update OAuthController.php
    fix: $updateFields with no value type specified in iterable

--- src/Controllers/OAuthController.php
+++ src/Controllers/OAuthController.php
@@ -136,6 +136,13 @@ class OAuthController extends BaseController implements ControllersInterface
         return $findUser !== null;
     }
 
+    /**
+     * Syncs user information based on provided fields.
+     *
+     * @param array $find           Array containing criteria to find the user.
+     * @param array $updateFields   Fields to update for the user.
+     * @return int                  The ID of the user whose information is synced.
+     */
     private function syncingUserInfo(array $find = [], array $updateFields = []): int
     {
         $users = model('ShieldOAuthModel');
