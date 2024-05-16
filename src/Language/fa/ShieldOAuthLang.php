8de40d67326bcb8243d33fba535e7df7b687b1d2
Aselsan <syamsuddine9@gmail.com>
2024-05-17 04:16:17 +0700

    Update ShieldOAuthLang.php
    add language for 'account_not_found' key

--- src/Language/fa/ShieldOAuthLang.php
+++ src/Language/fa/ShieldOAuthLang.php
@@ -18,6 +18,7 @@ return [
     'Callback' => [
         'oauth_class_not_set' => 'خطای رخ داد، به نظر میرسد کلاس OAuth مورد نظر به درستی تنظیم نشده است.',
         'anti_forgery'        => 'متاسفانه، تلاش شما ، یک درخواست جعلی تشخیص داده شد.',
+        'account_not_found'   => '(To be translated) Your account not found. Please register first!',
     ],
 
     // ShieldOAuthButton in views
