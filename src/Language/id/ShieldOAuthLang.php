32df3dd6485b88c03e95de512ce8d01e7c59cb7b
Aselsan <syamsuddine9@gmail.com>
2024-05-17 22:27:33 +0700

    lang: [id] Added support language Indonesian
    * Added language Indonesian
    
    * fix: PHP Coding Standards

---
+++ src/Language/id/ShieldOAuthLang.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Shield OAuth.
+ *
+ * (c) Datamweb <pooya_parsa_dadashi@yahoo.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+// ShieldOAuthLang language file
+return [
+    // Errors List
+    'unknown'  => 'Ups, kesalahan tidak terduga!',
+    'Callback' => [
+        'oauth_class_not_set' => 'Terjadi kesalahan, sepertinya class OAuth tidak terpasang.',
+        'anti_forgery'        => 'Maaf, permintaan Anda terdeteksi tidak valid!',
+        'account_not_found'   => 'Akun tidak ditemukan, silakan mendaftar terlebih dahulu!',
+    ],
+
+    // ShieldOAuthButton in views
+    'other'       => 'Lainnya',
+    'login_by'    => 'Masuk dengan : ',
+    'register_by' => 'Registrasi dengan : ',
+
+    // Errors List For all OAuth
+    'Github' => [
+        'github'    => 'GitHub',
+        'not_allow' => 'Sekarang Anda tidak bisa masuk atau mendaftar dengan GitHub!',
+    ],
+    'Google' => [
+        'google'    => 'Google',
+        'not_allow' => 'Sekarang Anda tidak bisa masuk atau mendaftar dengan Google!',
+    ],
+    // 'Yahoo' => [
+    //     'yahoo'     => 'Yahoo',
+    //     'not_allow' => 'Sekarang Anda tidak bisa masuk atau mendaftar dengan Yahoo!',
+
+    // ],
+];
